9780c28fe314c1ea5eb9eeb5faaba0b11dd82c01
Stuart Hertzig <shertzig@gmail.com>
2015-05-24 23:42:22 -0700

    fixed whitespace.

--- magic8ball.py
+++ magic8ball.py
@@ -3,18 +3,18 @@ import random
 import sys
 
 class Magic8BallPlugin(plugintypes.TelegramPlugin):
-	"""
-	It's a magic eight ball!
-	"""
+    """
+    It's a magic eight ball!
+    """
 
-	patterns = ["^!8ball (.*)"]
-	usage = ["!8ball <question>"]
-	
-	std_8ball_responses = ["Maybe","Probably","Yes", "No","In you're dreams", "Not Likely", "That could be the case", "Absolutely", "Most definitely", "No way"]
-	rand_number = random.randint(0, len(std_8ball_responses))
+    patterns = ["^!8ball (.*)"]
+    usage = ["!8ball <question>"]
+    
+    std_8ball_responses = ["Maybe","Probably","Yes", "No","In you're dreams", "Not Likely", "That could be the case", "Absolutely", "Most definitely", "No way"]
+    rand_number = random.randint(0, len(std_8ball_responses))
 
-	def run(self, msg, matches):
-		try:
-			return std_8ball_responses[rand_number]
-		except:
-			return sys.exc_info()[0]
+    def run(self, msg, matches):
+        try:
+            return std_8ball_responses[rand_number]
+        except:
+            return sys.exc_info()[0]
