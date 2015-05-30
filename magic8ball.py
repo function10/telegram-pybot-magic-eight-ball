9a21b2e8554c5f6ed8909e2cce2170137d9c5ecf
Stuart Hertzig <shertzig@gmail.com>
2015-05-30 01:21:32 -0700

    removed qwords and logic related to it. this will allow anything that is not a where,what why.. etc question

--- magic8ball.py
+++ magic8ball.py
@@ -31,16 +31,13 @@ class Magic8BallPlugin(plugintypes.TelegramPlugin):
             ]
         
     def run(self, msg, matches):
-        qwords = ["will", "am", "is", "does", "are", "shall", "isn't","can"]
         bad_qwords = ["who", "what", "where", "when", "why", "how"]
         first_word = matches.group(1).lower().split()[0]
 
         try:
-            if first_word in qwords:
-                return self.responses[randrange(len(self.responses))]
-            elif first_word in bad_qwords:
+            if first_word in bad_qwords:
                 return "Those are questions I'm not sure how to answer. Perhaps you can ask Google."
             else:
-                return "I'm not even sure that was a question!"
+                return self.responses[randrange(len(self.responses))]
         except:
             return sys.exc_info()[0]
